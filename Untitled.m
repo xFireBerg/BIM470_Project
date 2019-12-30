00e7836baca90f6dcec7a665af15cb893f43c705
xFireBerg <zabi_ghafoory@hotmail.com>
2019-12-31 00:52:30 +0300

    added simple maxpooling

--- Untitled.m
+++ Untitled.m
@@ -40,12 +40,31 @@ C = zeros(50, 50); %needs to change according to image, filter, and stride size
 % theSum = sum(multiple, 'all');
 % C(1,1) = theSum;
 
+% operations for creating a convolution layer from a single image and
+% filter with window size 2*2 and stride 1
 for row = 1:49
     for col = 1:49
         window = image(row:row+1, col:col+1);
-        multiple = window.*filter;
-        theSum = sum(multiple, 'all');
-        C(row,col) = theSum;
+        multiple = window.*filter; %element-wise multiplication
+        theSum = sum(multiple, 'all'); %sum of all elements within window
+        C(row,col) = theSum; 
     end
 end
 
+% operations for maxpooling C layer using window size 2*2 and stride 2
+maxLayer = zeros(25, 25, 2); % maxpool layer
+i = 1;
+for row = 1:2:49
+    j = 1;
+    for col = 1:2:49
+        window = C(row:row+1, col:col+1); % sliding window
+        [maxElement, maxIdx] = max(window, [], 'all', 'linear'); % find max of window with idx
+        maxLayer(i, j, 1) = maxElement; % set max elements on first 'page'
+        maxLayer(i,j,2) = maxIdx; % set max element idx on second 'page'
+        j = j + 1; 
+    end
+    i = i + 1;
+end
+
+
+        
