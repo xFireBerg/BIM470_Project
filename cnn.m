5fe41d069a8b4cdb7ff4700186a8af8c5fca98e7
ahmet gazi <ahmetgazi03@gmail.com>
2020-01-02 23:53:29 +0300

    Sout classfy

--- cnn.m
+++ cnn.m
@@ -24,11 +24,13 @@ disp(i);
 j = mod(i,20);
 image = inpM(:,:,:,i);
 
-if (i >= 20)
+if (i <= 20)
     class = 1;
-elseif (i > 20) && (i<=40)
+end
+if (i > 20) && (i<=40)
     class = 2;
-else
+end
+if (i > 40) && (i<=60)
     class = 3;
 end
 
@@ -38,6 +40,8 @@ end
 [Sinput,Shidden,Souts] = localGradientOut(class,Yout,Vout,W1,W2);
 %Vhidden = Vhidden(1:10);
 disp(Souts);
+disp(Vout);
+%disp(Sinput);
 [WW1,WW2] = updateWeight(W1,W2,Shidden,Souts,F,Vhidden,learningRate);
 
 [max_backprop] = poolBackprop(maxLayer,Sinput);
@@ -51,23 +55,7 @@ W2 = WW2;
 
 end
 
-% image = inpM(:,:,:,2);
-% learningRate = 0.1;
-% % initialize -- we need to give the flatten matrix size as an input
-% [W1,W2,bias] = initialize(3125,10,3);
-% % Forward 
-% [Yout,Vhidden,Vout,maxLayer,F,filter,C] = forward(W1,W2,bias,image);
-% 
-% [Sinput,Shidden,Souts] = localGradientOut(1,Yout,Vout,W1,W2);
-% %Vhidden = Vhidden(1:10);
-% 
-% [WW1,WW2] = updateWeight(W1,W2,Shidden,Souts,F,Vhidden,learningRate);
-% 
-% [max_backprop] = poolBackprop(maxLayer,Sinput);
-% 
-% [convBack] = poolToCnv(C,max_backprop,size(filter));
-% 
-% [updatedFilter] = updateFilter(image,convBack,filter,learningRate);
-
-% always gives y2 with 1 prob, others very very low
+[Yout,Vhidden,Vout,maxLayer,F,filter,C] = forward(W1,W2,bias,inpM(:,:,:,1),filter);
+disp("asd");
+disp(Yout);
 
--- localGradientOut.m
+++ localGradientOut.m
@@ -9,19 +9,23 @@ y3=Yout(3);
 S = (exp(v1)+exp(v2)+exp(v3));
 
 
+% Sout1 = -(((-1)/y1)*((exp(v1)*(S-exp(v1))) / S^2));
+% Sout2 = -(((-1)/y1)*(-(exp(v2)*(S-exp(v2))) / S^2));
+% Sout3 = -(((-1)/y1)*(-(exp(v3)*(S-exp(v3))) / S^2));
+%asdasda asdas aa a a a
 if correctClass == 1
 Sout1 = -(((-1)/y1)*((exp(v1)*(S-exp(v1))) / S^2));
-Sout2 = -(((-1)/y1)*((exp(v2)*(S-exp(v2))) / S^2));
-Sout3 = -(((-1)/y1)*((exp(v3)*(S-exp(v3))) / S^2));
+Sout2 = -(((-1)/y1)*(-(exp(v1)*exp(v2)*exp(v3)) / S^2));
+Sout3 = -(((-1)/y1)*(-(exp(v1)*exp(v2)*exp(v3)) / S^2));
 end
 if correctClass == 2
-Sout1 = -(((-1)/y2)*(-(exp(v1)*exp(v2)) / S^2));
+Sout1 = -(((-1)/y2)*(-(exp(v1)*exp(v2)*exp(v3)) / S^2));
 Sout2 = -(((-1)/y2)*((exp(v2)*(S-exp(v2))) / S^2));
-Sout3 = -(((-1)/y2)*((exp(v3)*(S-exp(v3))) / S^2));
+Sout3 = -(((-1)/y2)*(-(exp(v1)*exp(v2)*exp(v3)) / S^2));
 end
 if correctClass == 3
-Sout1 = -(((-1)/y3)*(-(exp(v1)*exp(v2)) / S^2));
-Sout2 = -(((-1)/y3)*((exp(v2)*(S-exp(v2))) / S^2));
+Sout1 = -(((-1)/y3)*(-(exp(v1)*exp(v2)*exp(v3))/ S^2));
+Sout2 = -(((-1)/y3)*(-(exp(v1)*exp(v2)*exp(v3)) / S^2));
 Sout3 = -(((-1)/y3)*((exp(v3)*(S-exp(v3))) / S^2));
 end
 
